692e2367524fc2f6a657cdf39f0838f72952bd01
Rene Füchtenkordt <rene@migranthire.com>
2016-11-05 16:47:23 +0100

    it works

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,7 +7,12 @@
 
     /** @ngInject */
     function MainController($scope) {
-        $scope.getNumber = getNumber
-         
+        console.log("test")
+        $scope.luckyFive =function() {
+            $scope.data= Math.floor(Math.random() * 7)
+            if($scope.data==5){
+                $scope.result="🤑🤑🤑You Win🤑🤑🤑"
+            } else {$scope.result="😥😥😥You Loose😥😥😥"}
+        };
     }
 })();
